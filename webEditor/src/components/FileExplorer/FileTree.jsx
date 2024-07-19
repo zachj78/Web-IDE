ee8f8f1e4ac8016ff5040374fd22f10f685747b0
Zach Johnson <zachjohnson7887@gmail.com>
2024-07-19 16:10:57 -0400

    Style Changes

--- webEditor/src/components/FileExplorer/FileTree.jsx
+++ webEditor/src/components/FileExplorer/FileTree.jsx
@@ -1,35 +1,62 @@
-import { Box, List, ListItem } from '@chakra-ui/react';
-import React, { useContext, useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { List, ListItem, Box } from '@chakra-ui/react';
+import { FaChevronRight, FaChevronDown } from 'react-icons/fa';
 import { ActiveFileContext, FileDirectoryContext } from '../../context/IDEContext';
+import "../../styles/FileTree.css"
 
 const FileTree = () => {
-    const { files } = useContext(FileDirectoryContext);
-    const { activeFile } = useContext(ActiveFileContext);
+  const { files } = useContext(FileDirectoryContext);
+  const { activeFile, setActiveFile } = useContext(ActiveFileContext);
+  const [collapsed, setCollapsed] = useState({});
 
-    const renderDirectory = (files) => {
-        return (
-            <List spacing={2} styleType="none" id="list">
-                {Object.keys(files).map((key) => (
-                    <ListItem key={key}>
-                        {typeof files[key] === "object" ? (
-                            <Box pl={4} borderLeft="1px solid black">
-                                <Box fontWeight="bold" mb={2}>{key}</Box>
-                                {renderDirectory(files[key])}
-                            </Box>
-                        ) : (
-                            <Box pl={4}>{key}</Box>
-                        )}
-                    </ListItem>
-                ))}
-            </List>
-        );
-    };
+  const toggleCollapse = (key) => {
+    setCollapsed(prevCollapsed => ({
+      ...prevCollapsed,
+      [key]: !prevCollapsed[key],
+    }));
+  };
 
+  const renderDirectory = (files, parentKey = '') => {
     return (
-        <Box w="20vw" overflowY="auto" fontSize="14px" maxH="20vh">
-            {files ? renderDirectory(files) : <p>No files uploaded yet</p>}
-        </Box>
+      <List spacing={2} styleType="none" id="list">
+        {Object.keys(files).map((key) => {
+          const path = `${parentKey}${key}`;
+          const isCollapsed = collapsed[path] || false;
+          const hasChildren = typeof files[key] === 'object';
+
+          return (
+            <ListItem key={path}>
+              {hasChildren ? (
+                <Box pl={4} borderLeft="1px solid black">
+                  <Box 
+                    fontWeight="bold" 
+                    mb={2} 
+                    display="flex" 
+                    alignItems="center"
+                    onClick={() => toggleCollapse(path)}
+                    cursor="pointer"
+                  >
+                    {isCollapsed ? <FaChevronRight /> : <FaChevronDown />} {key}
+                  </Box>
+                  {!isCollapsed && renderDirectory(files[key], `${path}/`)}
+                </Box>
+              ) : (
+                key === activeFile ? 
+                  <Box pl={4} bgColor="#444444">{key}</Box> :
+                  <Box cursor="pointer" className="file-name" pl={4} onClick={() => setActiveFile(key)}>{key}</Box> 
+              )}
+            </ListItem>
+          );
+        })}
+      </List>
     );
+  };
+
+  return (
+    <Box className="file-display" w="20vw" overflowY="auto" fontSize="14px" maxH="20vh">
+    { files ? renderDirectory(files) : <div>No Files Uploaded Yet!</div> }
+    </Box>
+  )
 };
 
 export default FileTree;
