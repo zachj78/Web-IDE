f08a2ae7a3c5c1184b882f7060e2df8f35527d1c
Zach Johnson <zachjohnson7887@gmail.com>
2024-07-20 14:40:51 -0400

    Added SelecteFileContext - controls selected files from activefilebar

--- webEditor/src/context/IDEContext.jsx
+++ webEditor/src/context/IDEContext.jsx
@@ -6,25 +6,29 @@ export const FileDirectoryContext = createContext();
 export const ExplorerErrorHandler = createContext();
 export const CachedFileArrayContext = createContext();
 export const ClickedFileContext = createContext();
+export const SelectedFileContext = createContext();
 
 //Create provider Components
 export const IDEProvider = ({ children }) => {
-    const [activeFiles, setActiveFiles] = useState(null);
+    const [activeFiles, setActiveFiles] = useState([]);
     const [files, setFiles] = useState(null);
     const [explorerErrorHandler, setExplorerErrorHandler] = useState(null);
     const [cachedFileArray, setCachedFileArray] = useState(null);
     const [clickedFiles, setClickedFiles] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
 
     return (
         <FileDirectoryContext.Provider value={{ files, setFiles }}>
             <ActiveFileContext.Provider value={{ activeFiles, setActiveFiles }}>
-                <ClickedFileContext.Provider value={{ clickedFiles, setClickedFiles }}>
-                    <ExplorerErrorHandler.Provider value={{ explorerErrorHandler, setExplorerErrorHandler }}>
-                        <CachedFileArrayContext.Provider value={{ cachedFileArray, setCachedFileArray }}>
-                            { children }
-                        </CachedFileArrayContext.Provider>
-                    </ExplorerErrorHandler.Provider>
-                </ClickedFileContext.Provider>
+                <SelectedFileContext.Provider value={{ selectedFile, setSelectedFile }}>
+                    <ClickedFileContext.Provider value={{ clickedFiles, setClickedFiles }}>
+                        <ExplorerErrorHandler.Provider value={{ explorerErrorHandler, setExplorerErrorHandler }}>
+                            <CachedFileArrayContext.Provider value={{ cachedFileArray, setCachedFileArray }}>
+                                { children }
+                            </CachedFileArrayContext.Provider>
+                        </ExplorerErrorHandler.Provider>
+                    </ClickedFileContext.Provider>
+                </SelectedFileContext.Provider>
             </ActiveFileContext.Provider>
         </FileDirectoryContext.Provider>
     )
