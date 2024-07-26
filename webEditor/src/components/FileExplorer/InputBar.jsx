8d6ed383f725e4d32c976fdabe667e6c8f355780
Zach Johnson <zachjohnson7887@gmail.com>
2024-07-26 12:30:40 -0400

    Implemented file system access api

--- webEditor/src/components/FileExplorer/InputBar.jsx
+++ webEditor/src/components/FileExplorer/InputBar.jsx
@@ -1,224 +1,119 @@
 import React, { useContext, useEffect } from 'react';
 import { Box, Tooltip } from '@chakra-ui/react';
 import { FaFolder } from 'react-icons/fa';
-import {
-  CachedFileArrayContext,
-  FileDirectoryContext,
-} from '../../context/IDEContext';
-import { ExplorerErrorHandler } from '../../context/IDEContext';
+import { FileDirectoryContext, ExplorerErrorHandler, FileHandleArrayContext, DirectoryHandleArrayContext } from '../../context/IDEContext';
 import { FaFile } from 'react-icons/fa6';
 
 const InputBar = () => {
   const { files, setFiles } = useContext(FileDirectoryContext);
   const { setExplorerErrorHandler } = useContext(ExplorerErrorHandler);
-  const { cachedFileArray, setCachedFileArray } = useContext(
-    CachedFileArrayContext,
-  );
-  let directoryAction = null;
-  let root = {};
+  const { fileHandles, setFileHandles } = useContext(FileHandleArrayContext);
+  const { setDirectoryHandles } = useContext(DirectoryHandleArrayContext);
 
-  const searchFileDirectory = (file) => {
-    console.log(
-      'searching file directory, existing dir: ',
-      files,
-      ' for file(s): ',
-      file,
-    );
-    for (let i = 0; i < file.length; i++) {
-      const parts = file[i].webkitRelativePath.split('/');
-      let fileExists = false;
+  useEffect(() => {
+    console.log('files updated: ', files);
+  }, [files]);
 
-      parts.forEach((part, index) => {
-        function recursiveKeySearch(files, part) {
-          console.log('FILE CONTENT : ', files);
-          for (let key in files) {
-            console.log('dir keys: ', key);
-            console.log('added folder parts ', part, 'index --', index);
-            if (key === part) {
-              fileExists = true;
-              setExplorerErrorHandler("Can't add file - Already added");
-              console.log("Can't add file - Already added");
-              return;
-            }
-
-            if (typeof files[key] === 'object') {
-              recursiveKeySearch(files[key], part);
-            }
-          }
-        }
-        recursiveKeySearch(files, part);
+  const handleFileUpload = async () => {
+    try {
+      console.log('Opening file picker...');
+      const [fileHandle] = await window.showOpenFilePicker({
+        multiple: false
       });
 
-      if (fileExists) {
-        return;
+      if (fileHandle) {
+        console.log('File selected:', fileHandle.name);
+        const file = await fileHandle.getFile();
+        console.log('File object:', file);
+        const fileData = await file.text();
+        console.log('File data:', fileData);
+
+        setFiles((prevFiles) => ({
+          ...prevFiles,
+          [file.name]: fileData
+        }));
+        setFileHandles((prevHandles) => ({
+          ...prevHandles,
+          [fileHandle.name]: fileHandle
+        }));
+      } else {
+        console.log('No file selected.');
       }
-    }
-
-    console.log('Folder not already uploaded, uploading');
-    addToDirectory(file);
-  };
-
-  const createUniqueFileArray = (fileArray) => {
-    if (cachedFileArray) {
-      //merge existing files and new files
-      const mergedFiles = [...cachedFileArray, ...fileArray];
-      const uniqueFiles = Array.from(new Set(mergedFiles));
-      setCachedFileArray(uniqueFiles);
-    } else {
-      setCachedFileArray(fileArray);
+    } catch (err) {
+      console.error('Error uploading file:', err);
+      setExplorerErrorHandler('Error uploading file, try again');
     }
   };
 
-  const handleInputClick = (e) => {
-    e.preventDefault();
-    const openFolderInput = document.getElementById('folder-input');
-    const openFileInput = document.getElementById('file-input');
-
-    let targetElement = e.target;
-    while (targetElement && !targetElement.id) {
-      targetElement = targetElement.parentElement;
-    }
-
-    if (!targetElement) {
-      console.error('No target element with an id found');
-      return;
-    }
-
-    console.log('element id clicked: ', targetElement.id);
-
-    switch (targetElement.id) {
-      case 'open-folder':
-        console.log('Open Folder Button Clicked');
-        openFolderInput.click();
-        directoryAction = targetElement.id;
-        break;
-      case 'open-file':
-        console.log('Open File Button Clicked');
-        openFileInput.click();
-        directoryAction = targetElement.id;
-        break;
-      default:
-        return;
+  const handleDirectoryUpload = async () => {
+    try {
+      console.log('Opening directory picker...');
+      const directoryHandle = await window.showDirectoryPicker();
+      console.log('Directory selected:', directoryHandle.name);
+      const { structure, handles } = await parseDirectory(directoryHandle);
+      console.log('Directory structure:', structure);
+      setFiles((prevFiles) => ({
+        ...prevFiles,
+        [directoryHandle.name]: structure
+      }));
+      setFileHandles((prevHandles) => [...prevHandles, ...handles]);
+      setDirectoryHandles((prevHandles) => ({
+        ...prevHandles,
+        [directoryHandle.name] : directoryHandle,
+      }));
+    } catch (err) {
+      console.error('Error accessing file system', err);
+      setExplorerErrorHandler('Error uploading files, try again');
     }
   };
 
-  const processInput = (e) => {
-    const file = Array.from(e.target.files);
-    console.log('file info: ', file);
+  const parseDirectory = async (directoryHandle) => {
+    const directory = {};
+    const handles = [];
+    const chunkSize = 10;
 
-    if (file.length > 0) {
-      //creating unique file array
-      createUniqueFileArray(file);
-      switch (directoryAction) {
-        case 'open-folder':
-          console.log(
-            'action: open folder, uploaded folder, ',
-            file,
-            ' existing directory, ',
-            files,
-          );
-          files ? searchFileDirectory(file) : buildDirectoryObject(file);
-          break;
-        case 'open-file':
-          console.log(
-            'action: open file, uploaded folder, ',
-            file,
-            ' existing directory, ',
-            files,
-          );
-          files ? searchFileDirectory(file) : buildDirectoryObject(file);
-          break;
-        default:
-          return;
+    console.log('Parsing directory:', directoryHandle.name);
+
+    const directoryIterator = directoryHandle.entries();
+
+    while (true) {
+      const chunk = [];
+      for (let i = 0; i < chunkSize; i++) {
+        const result = await directoryIterator.next();
+        if (result.done) break;
+        chunk.push(result.value);
       }
-    } else {
-      setExplorerErrorHandler('Error! File/Folder must be chosen');
-    }
 
-    e.target.value = null;
-  };
+      if (chunk.length === 0) break;
 
-  const buildDirectoryObject = (fileArray) => {
-    console.log('directory obj being built, fileArray:', fileArray);
-    if (fileArray.length === 1) {
-      console.log('FILE UPLOADED');
-      let current = root;
-      current[fileArray[0].name] = 'file';
-      setFiles(root);
-    } else {
-      for (let i = 0; i < fileArray.length; i++) {
-        const parts = fileArray[i].webkitRelativePath.split('/');
-        let current = root;
-
-        parts.forEach((part, index) => {
-          if (!current[part]) {
-            if (index === parts.length - 1) {
-              current[part] = 'file';
-            } else {
-              current[part] = {};
-            }
-          }
-          current = current[part];
-        });
-      }
-      console.log('root', JSON.stringify(root, null, 2));
-      setFiles(root);
-    }
-  };
-
-  const addToDirectory = (fileArray) => {
-    console.log('FILE ADD ARRAY ::', fileArray[0].name);
-    if (fileArray.length === 1) {
-      console.log('Singular file uploaded');
-      const fileName = { [fileArray[0].name]: 'file' };
-      setFiles((prevState) => ({ ...prevState, ...fileName }));
-    }
-
-    for (let i = 0; i < fileArray.length; i++) {
-      const parts = fileArray[i].webkitRelativePath.split('/');
-      let current = root;
-
-      parts.forEach((part, index) => {
-        if (!current[part]) {
-          if (index === parts.length - 1) {
-            current[part] = 'file';
-          } else {
-            current[part] = {};
-          }
+      await Promise.all(chunk.map(async ([name, handle]) => {
+        if (handle.kind === 'file') {
+          directory[name] = 'file';
+          handles.push(handle);
+        } else if (handle.kind === 'directory') {
+          const result = await parseDirectory(handle);
+          directory[name] = result.structure;
+          handles.push(...result.handles);
         }
-        current = current[part];
-      });
+      }));
+
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    
     }
-    console.log('adding files !!!', JSON.stringify(root, null, 2));
-    setFiles((prevState) => ({ ...prevState, ...root }));
+
+    return { structure: directory, handles };
   };
 
   return (
     <Box w="20vw" border="1px solid #333" borderRadius={4}>
-      <input
-        type="file"
-        id="folder-input"
-        webkitdirectory="true"
-        directory=""
-        multiple
-        style={{ display: 'none', height: '0vh' }}
-        onChange={processInput}
-      />
       <Tooltip label="Open Folder" aria-label="open folder tooltip">
-        <button onClick={handleInputClick} className="open-folder-button">
+        <button onClick={handleDirectoryUpload} className="open-folder-button">
           <FaFolder size="1.3em" id="open-folder" />
         </button>
       </Tooltip>
 
-      <input
-        type="file"
-        id="file-input"
-        multiple
-        style={{ display: 'none' }}
-        onChange={processInput}
-      />
       <Tooltip label="Open File" aria-label="open file tooltip">
-        <button onClick={handleInputClick} className="open-file-button">
+        <button onClick={handleFileUpload} className="open-file-button">
           <FaFile size="1.2em" id="open-file" />
         </button>
       </Tooltip>
