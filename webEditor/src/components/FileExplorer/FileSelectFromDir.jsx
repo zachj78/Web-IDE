9cc17d08bf223b5aacad85611f0f284bae08646f
Zach Johnson <zachjohnson7887@gmail.com>
2024-07-29 18:47:27 -0400

    Created modal component to handle file selection from a parent folder

---
+++ webEditor/src/components/FileExplorer/FileSelectFromDir.jsx
@@ -0,0 +1,95 @@
+import { Box } from '@chakra-ui/react';
+import React, { useEffect, useState, useContext } from 'react';
+import CenteredModal from '../ReusableComponents/ReusableModal';
+import { FileDirectoryContext, ExplorerErrorHandler, FileHandleArrayContext, DirectoryHandleArrayContext } from '../../context/IDEContext';
+
+const FileSelectFromDir = ({ parentHandle, dirFileHandles }) => {
+    const { setFiles } = useContext(FileDirectoryContext);
+    const { setExplorerErrorHandler } = useContext(ExplorerErrorHandler);
+    const { setFileHandles } = useContext(FileHandleArrayContext);
+    const { setDirectoryHandles } = useContext(DirectoryHandleArrayContext);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    useEffect(() => {
+        if (dirFileHandles && dirFileHandles.length > 0) {
+            setModalOpen(true);
+        }
+    }, [dirFileHandles]);
+
+    const handleCloseModal = () => setModalOpen(false);
+
+    const handleSelectedFile = async (fileHandle) => {
+        try {
+          const file = await fileHandle.getFile();
+          const fullFilePath = `${parentHandle.name}/${file.name}`;
+          setFiles((prevFiles) => {
+            if (prevFiles[parentHandle.name]) {
+              return {
+                ...prevFiles,
+                [parentHandle.name]: {
+                  ...prevFiles[parentHandle.name],
+                  [fileHandle.name]: 'file', // Add or update the file
+                },
+              };
+            } else {
+              return {
+                ...prevFiles,
+                [parentHandle.name]: {
+                  [fileHandle.name]: 'file',
+                },
+              };
+            }
+          });
+          
+      
+          setFileHandles((prevHandles) => ({
+            ...prevHandles,
+            [fullFilePath]: fileHandle,
+          }));
+
+          setDirectoryHandles((prevHandles) => {
+            if(!prevHandles[parentHandle.name]) {
+                return {
+                    [parentHandle.name]: parentHandle
+                }
+            }
+          })
+
+        } catch (err) {
+          console.error("Error reading selected file:", err);
+          setExplorerErrorHandler("Error reading file, try again");
+        }
+      };
+    
+      const structureAddedFile = () => {
+            const directory = {};
+            directory[parentHandle.name]
+      }
+
+    return (
+        <>
+            {dirFileHandles && dirFileHandles.length > 0 && (
+                <CenteredModal isOpen={modalOpen} onClose={handleCloseModal} title="- Choose A File -">
+                <Box overflowY="auto" maxHeight="400px" padding="4">
+                        {dirFileHandles.map((file, index) => (
+                            <Box
+                                key={index}
+                                padding="2"
+                                marginBottom="2"
+                                backgroundColor="gray.600"
+                                borderRadius="md"
+                                cursor="pointer"
+                                _hover={{ backgroundColor: 'gray.200' }}
+                                onClick={() => handleSelectedFile(file)}
+                            >
+                                {file.name}
+                            </Box>
+                        ))}
+                    </Box>
+                </CenteredModal>
+            )}
+        </>
+    );
+};
+
+export default FileSelectFromDir;
---
+++ webEditor/src/components/ReusableComponents/ReusableModal.jsx
@@ -0,0 +1,32 @@
+// CenteredModal.js
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+} from '@chakra-ui/react';
+
+const CenteredModal = ({ isOpen, onClose, title, children }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalBody>
+          {children}
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CenteredModal;
